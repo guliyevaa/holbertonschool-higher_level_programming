7ac8f9809c652dfd634cf6ac5485588460f1c250
guliyevaa <12545@holbertonstudents.com>
2025-11-19 14:21:31 +0000

    .

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    """Returns a new list where all occurrences of `search` are replaced by `replace`."""
     return [replace if item == search else item for item in my_list]
