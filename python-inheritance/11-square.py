6f062d47d135b7dbaab1473b80b1b0689d13b6d4
guliyevaa <12545@holbertonstudents.com>
2025-11-25 15:36:27 +0000

    .

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Square class that inherits from Rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size):
+        # Validate size using Rectangle's integer_validator
+        self.integer_validator("size", size)
+        # Private attribute
+        self.__size = size
+        # Call Rectangle constructor with width = height = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Return the square description [Square] width/height"""
+        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
