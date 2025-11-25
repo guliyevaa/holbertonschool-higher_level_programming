61b132e078fbe0fb01c94ebfbc247fa253c2aa55
guliyevaa <12545@holbertonstudents.com>
2025-11-25 15:54:02 +0000

    .

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module defining a MyList class that inherits from list."""
+
+
 class MyList(list):
     """MyList class that inherits from list."""
     def print_sorted(self):
