ef9b09459b791bbda8240d1e29b51b2115a52cea
guliyevaa <12545@holbertonstudents.com>
2025-11-25 14:54:58 +0000

    .

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Function returns True if obj inherits from a_class."""
 
+
 def inherits_from(obj, a_class):
     """Check if obj is an instance of class that inherited from a_class"""
     return issubclass(type(obj), a_class) and type(obj) is not a_class
