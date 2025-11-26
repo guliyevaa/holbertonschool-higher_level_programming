e717424d4057d268ce59a7a6d22c15ce3afa4e3a
guliyevaa <12545@holbertonstudents.com>
2025-11-26 13:50:23 +0000

    .

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 This module provides a function to read a text file and print its content.
 """
 
+
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its content to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
