01cb001f9873d5e2e55caa9ba189e4d8e6c37548
guliyevaa <12545@holbertonstudents.com>
2025-11-27 10:56:43 +0000

    .

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,6 +4,7 @@ Function that returns the dictionary description
 for JSON serialization of an object.
 """
 
+
 def class_to_json(obj):
     """Returns the dictionary description for JSON serialization"""
     return obj.__dict__
