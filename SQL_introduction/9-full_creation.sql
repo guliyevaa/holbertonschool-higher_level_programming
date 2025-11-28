789bdf7611e18b90c6a6d609bef449ffe681278c
guliyevaa <12545@holbertonstudents.com>
2025-11-28 14:08:59 +0000

    .

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- second table
+DROP TABLE second_table;
+CREATE TABLE second_table(
+id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(256),
+score INT
+);
+INSERT INTO second_table(id,name,score)
+VALUES (1,"John",10)
+(2,"Alex",3)
+(3,"Bob",14)
+(4,"George",8);
