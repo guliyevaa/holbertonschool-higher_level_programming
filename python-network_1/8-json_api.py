cb353094db96120b8735a29672b89f8bd5ea238c
guliyevaa <12545@holbertonstudents.com>
2025-12-01 13:38:17 +0000

    .

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Sends a POST request to search_user and prints the result"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    # Get the letter from command line or default to empty string
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+
+    url = "http://0.0.0.0:5000/search_user"
+    data = {"q": q}
+
+    try:
+        response = requests.post(url, data=data)
+        json_response = response.json()
+
+        if json_response:
+            print("[{}] {}".format(json_response.get("id"), json_response.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
