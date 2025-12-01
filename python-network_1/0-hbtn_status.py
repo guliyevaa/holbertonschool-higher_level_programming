b8d0d2b1657df21946b9c76fc2800340015e1130
guliyevaa <12545@holbertonstudents.com>
2025-12-01 12:49:48 +0000

    .

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-from urllib import request
+import urllib.request
 
-response = request.urlopen("https://intranet.hbtn.io/status")
-html = response.read()
-print(html)
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
