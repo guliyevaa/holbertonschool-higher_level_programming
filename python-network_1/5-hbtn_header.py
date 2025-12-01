886239a2d49687983b3303d2faf0e41e8001ee8b
guliyevaa <12545@holbertonstudents.com>
2025-12-01 13:27:08 +0000

    .

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status using requests"""
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+
+    response = requests.get(url)
+    body = response.text
+
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
